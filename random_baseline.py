f3e630969b3b2fa141a89cba1c474f5353cf087d
Crystal Chen <crystalchen@Crystals-MacBook-Pro-3.local>
2025-12-06 10:55:07 -0800

    Added random policy

---
+++ random_baseline.py
@@ -0,0 +1,23 @@
+import numpy as np
+import random
+from forward import A, Game
+from abc import ABC, abstractmethod
+
+class RandomBaseline:
+    """
+    A random policy to be used as a baseline comparison for our Forward Search implementation.
+    """
+
+    def __init__(self game: Game, uniform_belief: BeliefState):
+        self.game = game
+        self.uniform_belief = uniform_belief
+    
+    def run(self, belief_state: BeliefState) -> A:
+        """
+        Choose a random course from valid courses
+        """
+        valid_actions = self.game.actions(belief_state)
+        return random.sample(valid_actions) #randomly sample a possible quarter (collection of classes)
+
+
+
